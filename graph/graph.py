8214365446b53538902e669e1d2ebc2a155cda07
Danilo Moura <danilolmoura@gmail.com>
2020-10-27 00:34:53 -0300

    Add graph struct

---
+++ graph/graph.py
@@ -0,0 +1,77 @@
+class Vertice:
+    def __init__(self):
+        self._repr = ''
+        self._cabeca_lista_adjacencia = None
+
+
+class Adjacencia:
+    def __init__(self, vertice, peso):
+        self._vertice = vertice
+        self._peso = peso
+        self._proximo = None
+
+
+class Graph:
+    def __init__(self, total_vertices):
+        self._total_vertices = total_vertices
+        self._total_arestas = 0
+        self._adjacencias = [Vertice() for _ in range(total_vertices)]
+
+
+def cria_grafo(total_vertices):
+    grafo = Graph(total_vertices)
+
+
+    return grafo
+
+
+def cria_adjacencia(vertice, peso):
+    adjacencia = Adjacencia(vertice, peso)
+
+    return adjacencia
+
+
+def cria_aresta(grafo, vertice_inicial, vertice_final, peso):
+    if not grafo:
+        return False
+    if vertice_final < 0 or vertice_final >= grafo._total_vertices:
+        return False
+    if vertice_inicial < 0 or vertice_inicial >= grafo._total_vertices:
+        return False
+    
+    nova_adjacencia = cria_adjacencia(vertice_final, peso)
+    nova_adjacencia._proximo = grafo._adjacencias[vertice_inicial]._cabeca_lista_adjacencia
+
+    grafo._adjacencias[vertice_inicial]._cabeca_lista_adjacencia = nova_adjacencia
+    grafo._total_arestas += 1
+
+    return True
+
+
+def imprime_grafo(grafo):
+    print("Vertices: {} \nArestas: {}\n\n".format(
+        grafo._total_vertices, grafo._total_arestas))
+
+    for i in range(grafo._total_vertices):
+        adjacencia = grafo._adjacencias[i]._cabeca_lista_adjacencia
+        
+        texto = ["V{}: ".format(i)]
+
+        while(adjacencia):
+            texto.append("-> V{}({}) ".format(adjacencia._vertice, adjacencia._peso))
+
+            adjacencia = adjacencia._proximo
+
+        texto.append("\n")
+        print(''.join(texto))
+
+if __name__ == "__main__":
+    g = cria_grafo(5)
+    cria_aresta(g, 0, 1, 2)
+    cria_aresta(g, 1, 2, 4)
+    cria_aresta(g, 2, 4, 40)
+    cria_aresta(g, 2, 0, 12)
+    cria_aresta(g, 3, 1, 3)
+    cria_aresta(g, 4, 3, 8)
+
+    imprime_grafo(g)

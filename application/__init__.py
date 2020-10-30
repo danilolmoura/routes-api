118af12c7bff08619d2e41fffb951d19d6d53beb
Danilo Moura <danilolmoura@gmail.com>
2020-10-30 18:15:35 -0300

    Add index endpoint

--- application/__init__.py
+++ application/__init__.py
@@ -6,11 +6,15 @@ def create_app(config_name):
 
     # Configure flask app
     app = Flask(__name__, instance_relative_config=True)
-
     app.config.from_object(config_by_name[config_name])
 
     # Configure blueprints
     from application.route import api_routes
     app.register_blueprint(api_routes.bp, url_prefix='/route')
+
+    # Define main route
+    @app.route('/')
+    def index():
+        return '<a href="https://github.com/danilolmoura/routes-api#routes-api">Read the docs!</a>'
 
     return app
